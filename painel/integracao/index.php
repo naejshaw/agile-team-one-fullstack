a7cefa6a28f189b9c841c7630b5d4a7107dba892
JeanFDev <jeanjfra1@gmail.com>
2024-03-02 09:32:40 -0300

    ajustes

--- painel/integracao/index.php
+++ painel/integracao/index.php
@@ -3,7 +3,7 @@
 include('../../_core/_includes/config.php');
 // RESTRICT
 restrict(2);
-atualiza_estabelecimento( isset($_SESSION['estabelecimento']['id']), "online" );
+atualiza_estabelecimento( $_SESSION['estabelecimento']['id'], "online" );
 // SEO
 $seo_subtitle = "Integração";
 $seo_description = "";
@@ -16,7 +16,7 @@ include('../_layout/sidebars.php');
 include('../_layout/modal.php');
 
 global $db_con;
-$eid = isset($_SESSION['estabelecimento']['id']); //estabelecimento logado
+$eid = $_SESSION['estabelecimento']['id']; //estabelecimento logado
 $meudominio = $httprotocol.data_info("estabelecimentos",$_SESSION['estabelecimento']['id'],"subdominio").".".$simple_url;
 
 // Id do estabelecimento logado 
@@ -171,7 +171,7 @@ if ($formdata) {
 					<div class="form-field-default">
 
 						<label>URL de importação:</label>
-						<input id="copyme" type="text" value="<?php echo isset($meudominio); ?>/shopping.xml" DISABLED/>
+						<input id="copyme" type="text" value="<?php echo $meudominio; ?>/shopping.xml" DISABLED/>
 
 					</div>
 
@@ -179,7 +179,7 @@ if ($formdata) {
 
 					<div class="col-md-3">
 						<label></label>
-						<button class="fullwidth" data-clipboard-text="<?php echo isset($meudominio); ?>/shopping.xml">
+						<button class="fullwidth" data-clipboard-text="<?php echo $meudominio; ?>/shopping.xml">
 							<span>
 								<i class="lni lni-clipboard"></i> Copiar
 							</span>
